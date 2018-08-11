aaa8bf24fc95b8876083e5118db7a7baeec534dc
Cezary Kosinski <cezary.m.kosinski@gmail.com>
2018-08-11 23:12:41 +0200

    added sketch for rogue movement handling and bounds setting to map_context

--- models/map_context.py
+++ models/map_context.py
@@ -1,11 +1,12 @@
 from models import map as m
-
+from constants import MAP_CONTEXT_CONSTANTS as MCC
+from constants import ROGUE_CONSTANTS as RC
 
 class MapContext:
     """
     todo
     """
-    def __init__(self, id=0):
+    def __init__(self, id=0, rogue):
         """
         todo
         :param id:
@@ -13,14 +14,28 @@ class MapContext:
         self.id = id
         self.maps = {}
         self.current_map = None
+        self.rogue = rogue
 
-    def start(self):
+    def start(self, size):
+        """
+        todo
+        :return:
+        """
+        self.maps.update({(0, 0): m.Map((0, 0), self)})
+        map_response = self.access_map((0, 0))
+        
+
+    def start_with_rogue(self, rogue):
         """
         todo
         :return:
         """
         self.maps.update({(0, 0): m.Map((0, 0), self)})
         self.access_map((0, 0))
+        map_response = self.current_map.place_rogue(rogue.torch_size)
+        while rogue.torch_size:
+            rogue_response = self.rogue.make_move(map_response)
+            map_response = self.current_map.make_move(rogue_response)
 
     def access_map(self, map_id): #, position = None):
         """
@@ -40,31 +55,88 @@ class MapContext:
         map_keys = self.maps.keys()
         mid_up_left, mid_up, mid_up_right, mid_left, mid_right, mid_down_left, mid_down, mid_down_right = [(current_id[0] + x, current_id[1] + y) for x,y in [(-1, 1), (0, 1), (1,1), (-1, 0), (1, 0), (-1,-1), (0, -1), (1,-1)]]
 
-        if not mid_up_left in map_keys:
-            self.maps.update({mid_up: m.Map((mid_up, self, self.current_map.get_south_east_bound))})
-        if not mid_up in map_keys:
-            self.maps.update({mid_up: m.Map((mid_up, self, self.current_map.get_south_bound))})
-        if not mid_up_right in map_keys:
-            self.maps.update({mid_up: m.Map((mid_up, self, self.current_map.get_south_west_bound))})
-        if not mid_left in map_keys:
-            self.maps.update({mid_left: m.Map((mid_left, self, self.current_map.get_east_bound))})
-        if not mid_right in map_keys:
-            self.maps.update({mid_right: m.Map((mid_right, self, self.current_map.get_west_bound))})
-        if not mid_down_left in map_keys:
-            self.maps.update({mid_up: m.Map((mid_up, self, self.current_map.get_north_east_bound))})
-        if not mid_down in map_keys:
-            self.maps.update({mid_up: m.Map((mid_up, self, self.current_map.get_north_bound))})
-        if not mid_down_right in map_keys:
-            self.maps.update({mid_down: m.Map((mid_down, self, self.current_map.get_north_west_bound))})
+        if mid_up_left not in map_keys:
+            self.maps.update({mid_up: m.Map((mid_up_left, self))})
+        if mid_up not in map_keys:
+            self.maps.update({mid_up: m.Map((mid_up, self))})
+        if mid_up_right not in map_keys:
+            self.maps.update({mid_up: m.Map((mid_up_right, self))})
+        if mid_left not in map_keys:
+            self.maps.update({mid_left: m.Map((mid_left, self))})
+        if mid_right not in map_keys:
+            self.maps.update({mid_right: m.Map((mid_right, self))})
+        if mid_down_left not in map_keys:
+            self.maps.update({mid_up: m.Map((mid_down_left, self))})
+        if mid_down not in map_keys:
+            self.maps.update({mid_up: m.Map((mid_down, self))})
+        if mid_down_right not in map_keys:
+            self.maps.update({mid_down: m.Map((mid_down_right, self))})
 
-    def set_northeastbound(self, pos):
-    def set_northbound(self, pos):
-    def set_northwestbound(self, pos):
+#tried to generalize following methods, however it was saving only 1 line
+    def get_northeast_bound(self, pos):
+        new_pos = pos[0] + 1, pos[1] - 1
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_southwest_bound()
+        else:
+            bound = MCC['DEFAULT_CORNER_BOUND']
+        return bound    
 
-    def set_eastbound(self, pos):
-    def set_westbound(self, pos):
+    def get_north_bound(self, pos):
+        new_pos = pos[0] + 1, pos[1]
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_south_bound()
+        else:
+            bound = MCC['DEFAULT_HORIZONTAL_BOUND']
+        return bound    
 
-    def set_southeastbound(self, pos):
-    def set_southbound(self, pos):
-    def set_southwestbound(self, pos):
+    def get_northwest_bound(self, pos):
+        new_pos = pos[0] + 1, pos[1] + 1
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_southeast_bound()
+        else:
+            bound = MCC['DEFAULT_CORNER_BOUND']
+        return bound    
 
+
+    def get_east_bound(self, pos):
+        new_pos = pos[0], pos[1] + 1
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_west_bound()
+        else:
+            bound = MCC['DEFAULT_VERTICAL_BOUND']
+        return bound    
+    def get_west_bound(self, pos):
+        new_pos = pos[0], pos[1] - 1
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_east_bound()
+        else:
+            bound = MCC['DEFAULT_VERTICAL_BOUND']
+        return bound    
+
+
+    def get_southeast_bound(self, pos):
+        new_pos = pos[0] - 1, pos[1] + 1
+        if new_pos in self.maps.keys():
+            bound = self.maps[new_pos].get_northeast_bound()
+        else:
+            bound = MCC['DEFAULT_CORNER_BOUND']
+        return bound    
+
+    def get_south_bound(self, pos):
+            new_pos = pos[0] - 1, pos[1]
+            if new_pos in self.maps.keys():
+                bound = self.maps[new_pos].get_northeast_bound()
+            else:
+                bound = MCC['DEFAULT_CORNER_BOUND']
+            return bound    
+
+    def get_southwest_bound(self, pos):
+            new_pos = pos[0] - 1, pos[1] - 1
+            if new_pos in self.maps.keys():
+                bound = self.maps[new_pos].get_northeast_bound()
+            else:
+                bound = MCC['DEFAULT_CORNER_BOUND']
+            return bound    
+
+    def display(self):
+        pass
